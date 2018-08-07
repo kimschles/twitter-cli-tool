2e3009ecefade1315cc830410da52e65d8bb7f41
kimschles <schlesinger.kim@gmail.com>
2018-08-06 21:45:39 -0600

    fix formatting

--- twitter-server.js
+++ twitter-server.js
@@ -1,6 +1,6 @@
 var Twitter = require('twitter');
 
-require('dotenv').config()
+require('dotenv').config();
 
 var client = new Twitter({consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET});
 
